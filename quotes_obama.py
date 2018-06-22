f6ad539396e5c21ea29ff653e14af176eb4d4ef1
mzkang <0911kmj@naver.com>
2018-06-22 13:57:46 +0900

    2018-06-22

---
+++ quotes_obama.py
@@ -0,0 +1,15 @@
+#1.삼중 따옴표
+print('''President Barack Obama said,
+ "Don't just play on your phone,
+\tprogram it."''')
+
+
+#2.작은 따옴표
+print('President Barack Obama said,\
+\n "Don\'t just play on your phone,\
+\n\tprogram it."')
+      
+
+#3.큰 따옴표
+print("President Barack Ohbama said,\n \"Don't just play on your phone,\n\tprogram it.\"")
+      
